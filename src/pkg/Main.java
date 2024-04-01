f4de7759b46176c7a25580a2119a61b89f433a0c
Abood Nasr <29415627+AN-773@users.noreply.github.com>
2024-04-02 01:43:06 +0300

    Create Main.java

---
+++ src/pkg/Main.java
@@ -0,0 +1,38 @@
+/**
+* 1
+*/
+
+/**/package // pkg;
+  pkg;
+// asd
+/*0*/
+/*
+1
+2
+3
+*/
+
+class Main {
+/***/
+  Main(int x) {}
+
+  public Main() {}
+
+  /**/private/***/ Main/**/(double s) {}
+
+  @annotation Main(int i, int jk) {}
+
+  @annotation ////////
+  Main(double s, double a) {
+    /*
+1/**
+    */
+    System.out.println("///**/*/");
+  }
+
+  private void A() {}
+
+  public int B() {} Main BM() {}
+  
+  
+}
