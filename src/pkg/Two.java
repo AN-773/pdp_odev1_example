fe56747e414e3ed9251b8b6b3625c84ea549e240
Abood Nasr <29415627+AN-773@users.noreply.github.com>
2024-04-02 04:15:08 +0300

    Create Two.java

---
+++ src/pkg/Two.java
@@ -0,0 +1,51 @@
+/**
+* 1
+*/
+
+/**/package // pkg;
+  pkg;
+// asd
+/*0*/
+/*
+1
+2
+3
+*/
+
+ class// /**/Main// {
+   Main
+  {
+/***/
+  Main(int x) {}
+
+  public Main() {}
+
+  /**/private/***/ Main/**/(double s) {}
+
+  @annotation Main(int i, int jk) {}
+
+  @annotation ////////
+  Main(double s, double a) {
+    /*
+1/**
+    */
+    System.out.println("///**/*/");
+    System.out.println("");
+    System.out.println("");
+  }
+
+  private void A() {}
+
+  public int B() {} Main BM() {}
+ /*
+  */
+  /**
+  */
+
+  /*
+  /**
+  */
+
+  double X() {}
+  /**/
+}
