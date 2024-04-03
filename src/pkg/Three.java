490f0c6a68f2320aa17aaff61ceea711a209dc87
Abood Nasr <29415627+AN-773@users.noreply.github.com>
2024-04-03 04:08:55 +0300

    add three

---
+++ src/pkg/Three.java
@@ -0,0 +1,51 @@
+/**
+* 1
+*/
+
+/**/package // pkg;
+  pkg;
+// asd
+/*0*/
+/*
+1
+2
+3
+*/
+
+ class// /**/Main// {
+   Three<T,Y>
+  {
+/***/
+  Three(int x) {}
+
+  public Three() {}
+
+  /**/private/***/ Three/**/(double s) {}
+
+  @annotation Three(int i, int jk) {}
+
+  @annotation ////////
+  Three(double s, double a) {
+    /*
+1/**
+    */
+    System.out.println("///**/*/");
+    System.out.println("");
+    System.out.println("");
+  }
+
+  private List<String>A() {}
+
+  public List<Map<String,Integer>> B() {} Something<Map<String, Integer>  ,  Map< Integer , Map<String, Integer> > > BM() {}
+ /*
+  */
+  /**
+  */
+
+  /*
+  /**
+  */
+
+  double X() {}
+  /**/
+}
